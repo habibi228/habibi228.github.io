b5d5fc49d9688bc34765960398fa1662daef8a6d
habibi228 <150532069+habibi228@users.noreply.github.com>
2023-11-24 07:50:03 +0300

    Update hi.js

--- index_files/hi.js
+++ index_files/hi.js
@@ -2,8 +2,8 @@ let tg = window.Telegram.WebApp;
 
 tg.expand();
 
-tg.MainButton.textColor = '#FFFFFF';
-tg.MainButton.color = '#2cab37';
+tg.setBackgroundColor(#5b8698);
+tg.setHeaderColor(#5b8698);
 
 let item = "";
 
